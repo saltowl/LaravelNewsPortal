ba569adc55fcf747c41392d66b58f295545495f5
saltowl <vladislava-shakirova@mail.ru>
2019-04-12 13:34:57 +0300

    Add implementation of get and post in routes

--- NewsPortal/routes/web.php
+++ NewsPortal/routes/web.php
@@ -14,14 +14,29 @@ use App\Article;
 */
 
 Route::get('/', function () {
-    return view('articles');
+    return view('articles', [
+        'articles' => Article::orderBy('created_at', 'asc')->get()
+    ]);
 });
 
 /**
  * Add New Task
  */
 Route::post('/article', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'header' => 'required|max:300',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $article = new Article();
+    $article->header = $request->header;
+    $article->text = $request->text;
+    $article->save();
+
+    return redirect('/');
 });
 
 /**
