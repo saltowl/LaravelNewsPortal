5696ea5b2cb28f477c2f1a291006e9cbc0550251
saltowl <vladislava-shakirova@mail.ru>
2019-04-17 16:24:04 +0300

    Add view for articles' list

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -3,68 +3,30 @@
 @section('content')
 <div class="container">
     <div class="col-sm-offset-2 col-sm-8">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                New Article
-            </div>
 
-            <div class="panel-body">
-                <!-- Display Validation Errors -->
-                @include('common.errors')
-
-                <!-- New Article Form -->
-                <form action="{{ url('article')}}" method="POST" class="form-horizontal">
-                    {{ csrf_field() }}
-
-                    <!-- Article Name -->
-                    <div class="form-group">
-                        <label for="article-name" class="col-sm-3 control-label">Article</label>
-
-                        <div class="col-sm-6">
-                            <input type="text" name="name" id="article-name" class="form-control" value="{{ old('article') }}">
-                        </div>
-                    </div>
-
-                    <!-- Add Article Button -->
-                    <div class="form-group">
-                        <div class="col-sm-offset-3 col-sm-6">
-                            <button type="submit" class="btn btn-default">
-                                <i class="fa fa-btn fa-plus"></i>Add Article
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-        <!-- Current Articles -->
+        <!-- Articles -->
         @if (count($articles) > 0)
         <div class="panel panel-default">
             <div class="panel-heading">
-                Current Articles
+                Articles
             </div>
 
             <div class="panel-body">
                 <table class="table table-striped task-table">
-                    <thead>
-                        <th>Article</th>
-                        <th>&nbsp;</th>
-                    </thead>
                     <tbody>
                         @foreach ($articles as $article)
                         <tr>
-                            <td class="table-text">
-                                <div>{{ $article->name }}</div>
+                            <td>
+                                <div class="col-sm-12 col-md-12 col-lg-12 navbar-brand">{{ $article->header }}</div>
+                                <div class="col-sm-12 col-md-12 col-lg-12">{{ $article->text }}</div>
+
                             </td>
 
-                            <!-- Article Delete Button -->
+                            <!-- Article View Button -->
                             <td>
-                                <form action="{{ url('article/'.$article->id) }}" method="POST">
-                                    {{ csrf_field() }}
-                                    {{ method_field('DELETE') }}
-
-                                    <button type="submit" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-trash"></i>Delete
+                                <form action="{{ url('article/'.$article->id) }}" method="GET">
+                                    <button type="submit" class="btn btn-default" style="float: right;">
+                                        <i class="fa fa-btn fa-expand"></i>View
                                     </button>
                                 </form>
                             </td>
