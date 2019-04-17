c146c508f56faecc88d3f3900ba846626bd1ef84
saltowl <vladislava-shakirova@mail.ru>
2019-04-17 15:19:43 +0300

    Change fonts and add admin panel to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,14 +8,15 @@
     <title>News Portal</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
         body {
-            font-family: "Open Sans";
+            font-family: "Lato";
         }
 
         .fa-btn {
@@ -32,6 +33,10 @@
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
                     Article List
+                </a>
+
+                <a class="navbar-brand" style="float: right !important;" href="{{ url('/admin') }}">
+                    Admin Panel
                 </a>
             </div>
 
